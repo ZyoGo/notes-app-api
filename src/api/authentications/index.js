4a244c2b5c5e94f6e306734e2bd0f80e23a1cb04
w33h <academyynetworking@gmail.com>
2021-09-16 20:05:44 +0800

    Add Api Authentications

---
+++ src/api/authentications/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable object-curly-newline */
+const AuthenticationsHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'authentications',
+  version: '1.0.0',
+  register: async (server, { authenticationsService, usersService, tokenManager, validator }) => {
+    const authenticationHandler = new AuthenticationsHandler(
+      authenticationsService,
+      usersService,
+      tokenManager,
+      validator
+    );
+
+    server.route(routes(authenticationHandler));
+  },
+};
