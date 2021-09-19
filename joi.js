5a5a84a52de8395c80bf115d67c4a16179da451a
w33h <academyynetworking@gmail.com>
2021-09-19 14:58:35 +0800

    Reformat code using Prettier

--- joi.js
+++ joi.js
@@ -1,7 +1,93 @@
-const test = (test) => {
-  if (test) {
-    return 'hay';
-  }
+// User A
+const createUserARequest = {
+  url: 'http://localhost:5000/users',
+  method: 'POST',
+  header: {
+    'Content-Type': 'applications/json',
+  },
+  body: {
+    mode: 'raw',
+    raw: JSON.stringify({
+      username: 'user_a',
+      password: 'secret',
+      username: 'User A',
+    }),
+  },
 };
 
-test(true);
+pm.sendRequest(createUserARequest, (error, response) => {
+  console.log(error ? error : response);
+
+  // Setelah terdaftar, login dengan User A
+  const loginUserRequest = {
+    url: 'http://localhost:5000/authentications',
+    method: 'POST',
+    header: {
+      'Content-Type': 'applications/json',
+    },
+    body: {
+      mode: 'raw',
+      raw: JSON.stringify({
+        username: 'user_a',
+        password: 'secret',
+      }),
+    },
+  };
+
+  pm.sendRequest(loginUserRequest, (error, response) => {
+    // Masukkan access token user B ke environment variabel
+    if (!error) {
+      const {
+        data: { accessToken },
+      } = response.json();
+      pm.environment.set('accessTokenUserA', accessToken);
+    }
+  });
+});
+
+// User B
+const createUserBRequest = {
+  url: 'http://localhost:5000/users',
+  method: 'POST',
+  header: {
+    'Content-Type': 'applications/json',
+  },
+  body: {
+    mode: 'raw',
+    raw: JSON.stringify({
+      username: 'user_b',
+      password: 'secret',
+      fullname: 'User B',
+    }),
+  },
+};
+
+pm.sendRequest(createUserBRequest, (error, response) => {
+  console.log(error ? error : response);
+
+  // Setelah terdaftar, login dengan User B
+  const loginUserRequest = {
+    url: 'http://localhost:5000/authentications',
+    method: 'POST',
+    header: {
+      'Content-Type': 'applications/json',
+    },
+    body: {
+      mode: 'raw',
+      raw: JSON.stringify({
+        username: 'user_b',
+        password: 'secret',
+      }),
+    },
+  };
+
+  pm.sendRequest(loginUserRequest, (error, response) => {
+    if (!error) {
+      // Masukkan access token user B ke environment variabel
+      const {
+        data: { accessToken },
+      } = response.json();
+      pm.environment.set('accessTokenUserB', accessToken);
+    }
+  });
+});
