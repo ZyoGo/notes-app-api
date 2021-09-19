8eb29a6737a7198cb31b94591f9a232968a47e91
w33h <academyynetworking@gmail.com>
2021-09-19 14:57:24 +0800

    Add fiture Authorization

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -22,11 +22,14 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', tags, body } = request.payload;
+      // console.log(request.auth.credentials);
+      const { id: credentialId } = request.auth.credentials;
 
       const noteId = await this._service.addNote({
         title,
         tags,
         body,
+        owner: credentialId,
       });
 
       const response = h.response({
@@ -58,8 +61,10 @@ class NotesHandler {
     }
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
+  async getNotesHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+
+    const notes = await this._service.getNotes(credentialId);
     return {
       status: 'success',
       data: {
@@ -71,6 +76,9 @@ class NotesHandler {
   async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._service.verifyNoteOwner(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -102,7 +110,9 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
 
+      await this._service.verifyNoteOwner(id, credentialId);
       await this._service.editNoteById(id, request.payload);
 
       return {
@@ -133,7 +143,9 @@ class NotesHandler {
   async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
 
+      await this._service.verifyNoteOwner(id, credentialId);
       await this._service.deleteNoteById(id);
 
       return {
